5ada615fc26df04d46a1b3b8e4648e4273951244
Alcides Mignoso e Silva <alcidesmig@gmail.com>
2019-05-26 21:10:28 -0300

    Fazendo a lógica do Login

--- src/java/br/ufscar/dc/dsw/bean/SalaDeTeatroBean.java
+++ src/java/br/ufscar/dc/dsw/bean/SalaDeTeatroBean.java
@@ -2,8 +2,10 @@ package br.ufscar.dc.dsw.bean;
 
 import br.ufscar.dc.dsw.dao.DAOSalaDeTeatro;
 import br.ufscar.dc.dsw.dao.DAOSiteDeVenda;
+import br.ufscar.dc.dsw.dao.DAOUsuario;
 import br.ufscar.dc.dsw.pojo.SalaDeTeatro;
 import br.ufscar.dc.dsw.pojo.SiteDeVenda;
+import br.ufscar.dc.dsw.pojo.Usuario;
 import java.io.Serializable;
 import java.sql.SQLException;
 import java.util.List;
@@ -18,43 +20,60 @@ public class SalaDeTeatroBean implements Serializable {
 
     private SalaDeTeatro saladeteatro;
     private List<SalaDeTeatro> salas;
-    DAOSalaDeTeatro dao = new DAOSalaDeTeatro();
+    DAOSalaDeTeatro daoTeatro = new DAOSalaDeTeatro();
+    DAOUsuario daoUser = new DAOUsuario();
     int op;
     private String erro;
     private String operacao;
 
     public String gerenciar() {
-        op = 1;
-        salas = dao.getAll();
-        return "/views/templates_sala_de_teatro/gerenciar.xhtml";
+        if (BeanUsuario.getUserLogado().getIsAdmin()) {
+            op = 1;
+            salas = daoTeatro.getAll();
+            return "/views/templates_sala_de_teatro/gerenciar.xhtml";
+        } else {
+            return "403.xhtml";
+        }
     }
 
     public String lista() {
         op = 0;
-        salas = dao.getAll();
+        salas = daoTeatro.getAll();
         return "/views/templates_sala_de_teatro/lista.xhtml";
     }
 
     public String cadastra() {
-        saladeteatro = new SalaDeTeatro();
-        erro = "";
-        operacao = "Cadastro de Sala de Teatro";
-        return "/views/templates_sala_de_teatro/form.xhtml";
+        if (BeanUsuario.getUserLogado().getIsAdmin()) {
+            saladeteatro = new SalaDeTeatro();
+            erro = "";
+            operacao = "Cadastro de Sala de Teatro";
+            return "/views/templates_sala_de_teatro/form.xhtml";
+        } else {
+            return "403.xhtml";
+        }
     }
 
     public String edita(Long id) {
-        saladeteatro = dao.get(id);
+        saladeteatro = daoTeatro.get(id);
         operacao = "Edição de Sala de Teatro";
         return "form.xhtml";
     }
 
     public String salva() {
         if (saladeteatro.getId() == 0) {
-            dao.save(saladeteatro);
+            Usuario user = new Usuario(saladeteatro.getNome(), saladeteatro.getEmail(), saladeteatro.getSenha());
+            user.setIsSalaDeTeatro(true);
+            daoUser.save(user);
+            daoTeatro.save(saladeteatro);
         } else {
-            dao.update(saladeteatro);
+            Usuario user = daoUser.get(saladeteatro.getEmail());
+            user.setEmail(saladeteatro.getEmail());
+            user.setNome(saladeteatro.getNome());
+            user.setSenha(saladeteatro.getSenha());
+            daoUser.update(user);
+            daoTeatro.update(saladeteatro);
         }
-        salas = dao.getAll();
+        salas = daoTeatro.getAll();
         if (op == 1) {
             return "gerenciar.xhtml";
         }
@@ -62,13 +81,13 @@ public class SalaDeTeatroBean implements Serializable {
     }
 
     public String delete(Long id) {
-        dao.delete(dao.get(id)); //problema
-        salas = dao.getAll();
+        daoTeatro.delete(daoTeatro.get(id)); //problema
+        salas = daoTeatro.getAll();
         return "gerenciar.xhtml";
     }
 
     public String volta() {
-        salas = dao.getAll();
+        salas = daoTeatro.getAll();
         return "/views/templates_sala_de_teatro/lista.xhtml?faces-redirect=true";
     }
 
@@ -79,7 +98,7 @@ public class SalaDeTeatroBean implements Serializable {
     public String getSalaDeTeatroByName() throws SQLException {
         String nome = FacesContext.getCurrentInstance().
                 getExternalContext().getRequestParameterMap().get("nome");
-        saladeteatro = dao.getByNome(nome);
+        saladeteatro = daoTeatro.getByNome(nome);
         return "/views/templates_sala_de_teatro/lista.xhtml";
     }
 
@@ -117,7 +136,6 @@ public class SalaDeTeatroBean implements Serializable {
     public void setOperacao(String operacao) {
         this.operacao = operacao;
     }
-
 
     public void setSaladeteatro(SalaDeTeatro saladeteatro) {
         this.saladeteatro = saladeteatro;
